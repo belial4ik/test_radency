b1292ab52530a7ff34fb023f3f0dda619727f0dc
Belial <belyal4ik@gmail.com>
2022-01-13 16:24:05 +0300

    commit

---
+++ test.js
@@ -0,0 +1,10 @@
+function chooseBestSum(t, k, ls) {
+  let result =  ls.reduce((acum, value) => acum.concat(acum.filter(num => num.length < k).map(num => num.concat([value]))), [[]])
+      .filter(num => num.length === k)
+      .map(num => num.reduce((i, y) => i + y))
+      .filter(num => num <= t)
+      .sort((x, y) => y - x)[0]
+  return result ? result : null
+}
+
+console.log(chooseBestSum(174, 3, [51, 56, 58, 59, 61]))
